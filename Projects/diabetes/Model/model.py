3157b026d859a3d458a6902404b081c9a32b9a5d
Sathish.K <111336092+SwagAbyss@users.noreply.github.com>
2023-02-08 04:11:30 +0530

    commit

---
+++ Projects/diabetes/Model/model.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pickle
+
+# loading the saved model
+loaded_model = pickle.load(open('/Users/swag/Documents/GitHub/SwagML/Projects/diabetes/trained_model.sav', 'rb'))
+
+input_data = [4,110,92,0,0,37.6,0.191,30]
+
+# changing the input data into numpy array
+input_data_as_array = np.asarray(input_data)
+
+# reshaping the array as we are predicting for one instance
+input_data_reshape = input_data_as_array.reshape(1, -1)
+
+# predicting the label
+prediction = loaded_model.predict(input_data_reshape)
+# print(prediction)
+
+if prediction == 0:
+    print('The person is not Diabetic')
+else:
+    print('The person is Diabetic')
