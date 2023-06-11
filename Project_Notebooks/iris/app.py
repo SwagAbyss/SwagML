667d5b4a8a5ce3b5e5c45d3ffaac81a7f32c1425
John Doe <111336092+SwagAbyss@users.noreply.github.com>
2023-06-12 02:41:00 +0530

    Update app.py

--- Project_Notebooks/iris/app.py
+++ Project_Notebooks/iris/app.py
@@ -1,4 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import accuracy_score
+
 st.title("Hi Toolfe")
